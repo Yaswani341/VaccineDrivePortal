84606a590bee3e69b74b9ffbe3b3619591590452
Yaswani341 <yaswani.t@gmail.com>
2021-05-25 19:37:46 +0530

    Added STATUS page
    Displays all users information

---
+++ status.php
@@ -0,0 +1,57 @@
+<html>
+<body>
+<center>
+        <h1 style="background-color :antiquewhite;">
+                <p style="font-size:50px">Status page
+        </h1>
+        <style>
+                table 
+                {
+                        border-collapse: collapse;
+                        width: 100%;
+                }
+                th 
+                {
+                        background-color: #e7a192;
+                        color: white;
+                }
+                th, td 
+                {
+                        text-align: left;
+                        padding: 8px;
+                }
+                tr:nth-child(even)
+                {
+                        background-color: #f2f2f2
+                }
+
+        </style>
+        <?php
+                include "connect.php";
+                $sql = "SELECT * FROM user_details";
+                $result = $link->query($sql);
+                echo "<table border='1'>
+                <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Mobile</th>
+                <th>Dosage-1</th>
+                <th>Dosage-2</th>
+                </tr>";
+                while($row = mysqli_fetch_array($result))
+                {
+                        echo "<tr>";
+                        echo "<td>" . $row['name'] . "</td>";
+                        echo "<td>" . $row['address'] . "</td>";
+                        echo "<td>" . $row['contact'] . "</td>";
+                        echo "<td>" . $row['dos1'] . "</td>";
+                        echo "<td>" . $row['dose2'] . "</td>";
+                        echo "</tr>";
+                }
+                echo "</table>";
+                $link->close();
+        ?>
+        <a href="index.html">Click here to go back
+</center>
+</body>
+</html>
